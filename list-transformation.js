50fb398d34fd2f66038473ed32506dd7f6855472
Syed Ahmad <syed-ahmad@outlook.com>
2019-04-23 22:56:13 +0100

    Add _.map function

--- list-transformation.js
+++ list-transformation.js
@@ -1,4 +1,8 @@
 'use strict';
+// ============================
+// === _.each               ===
+// ============================
+
 var l = (item) => console.log(item);
 
 var _ = {};
@@ -17,20 +21,45 @@ _.each = function (list, callback) {
     }
   } 
 };
-
-//
-
 // _.each({ a: 1, b: [5,6,7], c: 3 }, (item) => console.log(item));
 // _.each(['Bilal', 'Shakira', 'Dee'], (item) => console.log(item));
 
 function CreateSuspectObject(name) {
-  return {
-    name: name,
-    color: name.split(' ')[1],
-    speak() { console.log(`My name is ${name}`); }
-  };
+  return { name: name, color: name.split(' ')[1], speak() { console.log(`My name is ${name}`); } };
 }
 
 var suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mr. White'];
+// Following works as _.each doesn't return anything :)
+console.log(_.each(suspects, CreateSuspectObject));
 
-console.log(_.each(suspects, CreateSuspectObject)); // Not working
+// ============================
+// === _.map                ===
+// ============================
+const weapons = ['candlestick', 'lead pipe', 'revolver'];
+const makeBroken = function (item) {
+  return `broken ${item}`;
+};
+
+// _.map returns an array
+_.map = function (list, iterator) {
+  if (!list && (typeof list !== 'object' || Array.isArray(list))) {
+    l(`list is not an object or array`);
+  }
+
+  const newArray = [];
+  if(!Array.isArray(list)) {
+    for (let key in list) {
+      newArray.push(iterator(list[key], key, list));
+    }
+  } else {
+    for (let i=0; i < list.length; i++) {
+      l(list[i]);
+      newArray.push(iterator(list[i], i, list));
+    }
+  }
+
+  return newArray;
+};
+
+const arr = _.map(weapons, makeBroken);
+l(arr);
